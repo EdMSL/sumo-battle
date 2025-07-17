9b4df9029d751716796a81434c9b385c5734a52e
Eduard Trait <eduard2011rus@gmail.com>
2025-07-17 15:40:06 +0700

    Update main menu UI

--- Assets/UI Toolkit/MainMenuUI.cs
+++ Assets/UI Toolkit/MainMenuUI.cs
@@ -1,31 +1,83 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenuUI : MonoBehaviour
 {
-    private VisualElement _container;
+    private VisualElement _menuContainer;
+    private VisualElement _settingsContainer;
     private VisualElement _btnsContainer;
     private VisualElement _difficultyContainer;
     private Button _playButton;
     private Button _settingsButton;
     private Button _backButton;
+    private Button _acceptButton;
+    private Button _cancelButton;
+    private Button _difficultyEasyButton;
+    private Button _difficultyNormalButton;
+    private Button _difficultyHardButton;
 
 
     private void OnEnable()
     {
         var uiDocument = GetComponent<UIDocument>();
 
-        _container = uiDocument.rootVisualElement.Q("menu-container");
-        _btnsContainer = _container.Q("btns-container");
-        _difficultyContainer = _container.Q("difficult-container");
+        _menuContainer = uiDocument.rootVisualElement.Q("menu-container");
+        _settingsContainer = uiDocument.rootVisualElement.Q("settings-container");
+        _btnsContainer = _menuContainer.Q("btns-container");
+        _difficultyContainer = _menuContainer.Q("difficult-container");
 
         _playButton = _btnsContainer.Q<Button>("play-btn");
         _settingsButton = _btnsContainer.Q<Button>("settings-btn");
         _backButton = _difficultyContainer.Q<Button>("back-btn");
+        _cancelButton = _settingsContainer.Q<Button>("cancel-btn");
+        _acceptButton = _settingsContainer.Q<Button>("accept-btn");
+        _difficultyEasyButton = _difficultyContainer.Q<Button>("easy-btn");
+        _difficultyNormalButton = _difficultyContainer.Q<Button>("normal-btn");
+        _difficultyHardButton = _difficultyContainer.Q<Button>("hard-btn");
 
         _playButton.RegisterCallback<ClickEvent>(OnPlayBtnClick);
         _backButton.RegisterCallback<ClickEvent>(OnBackBtnClick);
+        _settingsButton.RegisterCallback<ClickEvent>(OnSettingsBtnClick);
+        _cancelButton.RegisterCallback<ClickEvent>(OnCancelBtnClick);
+        _acceptButton.RegisterCallback<ClickEvent>(OnCancelBtnClick);
+        _difficultyEasyButton.RegisterCallback<ClickEvent>(OnDifficultyBtnClick);
+        _difficultyNormalButton.RegisterCallback<ClickEvent>(OnDifficultyBtnClick);
+        _difficultyHardButton.RegisterCallback<ClickEvent>(OnDifficultyBtnClick);
+    }
+
+    private void OnDifficultyBtnClick(ClickEvent evt)
+    {
+        var button = (Button)evt.target;
+        var btnName = button.name.Split('-')[0];
+
+        if (btnName == Save.DifficultyLevel.Easy.ToString().ToLower())
+        {
+            Save.SetDifficultyLevel(Save.DifficultyLevel.Easy);
+        }
+        else if (btnName == Save.DifficultyLevel.Normal.ToString().ToLower())
+        {
+            Save.SetDifficultyLevel(Save.DifficultyLevel.Normal);
+        }
+        else if (btnName == Save.DifficultyLevel.Hard.ToString().ToLower())
+        {
+            Save.SetDifficultyLevel(Save.DifficultyLevel.Hard);
+        }
+
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnCancelBtnClick(ClickEvent evt)
+    {
+        _settingsContainer.style.display = DisplayStyle.None;
+        _btnsContainer.style.display = DisplayStyle.Flex;
+    }
+
+    private void OnSettingsBtnClick(ClickEvent evt)
+    {
+        _btnsContainer.style.display = DisplayStyle.None;
+        _settingsContainer.style.display = DisplayStyle.Flex;
     }
 
     private void OnPlayBtnClick(ClickEvent evt)
@@ -44,5 +96,8 @@ public class MainMenuUI : MonoBehaviour
     {
         _playButton.UnregisterCallback<ClickEvent>(OnPlayBtnClick);
         _backButton.UnregisterCallback<ClickEvent>(OnBackBtnClick);
+        _settingsButton.UnregisterCallback<ClickEvent>(OnSettingsBtnClick);
+        _cancelButton.UnregisterCallback<ClickEvent>(OnCancelBtnClick);
+        _acceptButton.UnregisterCallback<ClickEvent>(OnCancelBtnClick);
     }
 }
