43932c1c425ea9311befa274172328a9aac08920
Eduard Trait <eduard2011rus@gmail.com>
2025-09-21 21:33:02 +0700

    Add Yandex sdk

---
+++ Assets/AdsYandex.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class AdsYandex : MonoBehaviour
+{
+    //   [SerializeField] private Level level;
+
+    [DllImport("__Internal")]
+    private static extern void ShowFullscreen();
+
+    [DllImport("__Internal")]
+    private static extern void ShowRewarded();
+
+    public void Show1()
+    {
+        ShowFullscreen();
+    }
+
+    public void Show2()
+    {
+        ShowRewarded();
+    }
+
+    public void AdsCoints()
+    {
+        // level.AfterSecondChance();
+    }
+}
